41f549cdc84495908de63a393bb1b478baac0e65
Nathan Fearnley <nfearnley@gmail.com>
2012-03-04 01:23:11 -0500

    Moved server info transaction code to ServerBrowser class

--- src/com/slugsource/steam/serverbrowser/ServerBrowser.java
+++ src/com/slugsource/steam/serverbrowser/ServerBrowser.java
@@ -4,19 +4,49 @@
  */
 package com.slugsource.steam.serverbrowser;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  *
  * @author Nathan Fearnley
  */
 public class ServerBrowser
 {
-    public static void testMethod(String a)
+    public static Server getServerInfo(InetAddress address, int port) throws SocketTimeoutException, NotAServerException, IOException
     {
-        System.out.println("String");
-    }
-    
-    public static void testMethod(Integer a)
-    {
-        System.out.println("Integer");
+        DatagramPacket response;
+        try
+        (DatagramSocket datagramSocket = new DatagramSocket()) {
+            datagramSocket.setSoTimeout(1000);
+
+            byte[] prefix =
+            {
+                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x54
+            };
+
+            byte suffix = 0x00;
+
+            byte[] sendBuffer = ArrayUtils.addAll(prefix, "Source Engine Query".getBytes());
+            sendBuffer = ArrayUtils.addAll(sendBuffer, suffix);
+
+            DatagramPacket request = new DatagramPacket(
+                    sendBuffer, sendBuffer.length, address, port);
+
+            datagramSocket.send(request);
+
+            byte[] receiveBuffer = new byte[1400];
+            response = new DatagramPacket(receiveBuffer, 1400);
+
+            datagramSocket.receive(response);
+        }
+        
+        ServerReader sReader = new ServerReader();
+        Server myServer = sReader.readServer(response.getData());
+        return myServer;
     }
 }
