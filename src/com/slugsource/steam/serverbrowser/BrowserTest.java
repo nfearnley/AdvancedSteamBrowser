a00a577c37f5e3638fecc31daf08da29109e4cec
Nathan Fearnley <nfearnley@gmail.com>
2012-03-05 22:19:31 -0500

    Wrote basic test GUI

--- src/com/slugsource/steam/serverbrowser/BrowserTest.java
+++ src/com/slugsource/steam/serverbrowser/BrowserTest.java
@@ -1,13 +1,17 @@
 package com.slugsource.steam.serverbrowser;
 
 import com.slugsource.steam.servers.KillingFloorServer;
+import com.slugsource.steam.servers.ServerAddress;
 import com.slugsource.steam.servers.query.KillingFloorGameQuery;
 import com.slugsource.steam.servers.query.KillingFloorPlayerQuery;
 import com.slugsource.steam.servers.query.KillingFloorServerQuery;
+import com.slugsource.steam.servers.query.MasterServerQuery;
 import com.slugsource.steam.servers.query.SourceServerQuery;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -21,16 +25,43 @@ public class BrowserTest
     private static KillingFloorServerQuery kfServerQuery = new KillingFloorServerQuery();
     private static KillingFloorGameQuery kfGameQuery = new KillingFloorGameQuery();
     private static KillingFloorPlayerQuery kfPlayerQuery = new KillingFloorPlayerQuery();
+    private static MasterServerQuery mQuery = new MasterServerQuery();
 
     public static void main(String[] args)
     {
         try
         {
-            sendRequest(InetAddress.getByName("68.232.176.139"), 28852);
-            sendRequest(InetAddress.getByName("localhost"), 28852);
+            sendMasterRequest(InetAddress.getByName("208.64.200.52"), 27011);
+//            sendRequest(InetAddress.getByName("68.232.176.139"), 28852);
+//            sendRequest(InetAddress.getByName("localhost"), 28852);
         } catch (Exception ex)
         {
             ex.printStackTrace();
+        }
+    }
+    
+    private static void sendMasterRequest(InetAddress address, int port) throws IOException
+    {
+        try
+        {
+            String header = "Server " + address.getHostAddress() + ":" + port;
+            header += '\n' + StringUtils.repeat('=', header.length());
+            System.out.println(header);
+
+            List<ServerAddress> serverList = new LinkedList<>();
+
+            mQuery.queryServer(address, port, "", serverList);
+
+            System.out.println(serverList);
+        } catch (SocketTimeoutException ex)
+        {
+            System.out.println("Could not contact server");
+        } catch (NotAServerException ex)
+        {
+            System.out.println("Response was not a server description");
+        } finally
+        {
+            System.out.println();
         }
     }
 
